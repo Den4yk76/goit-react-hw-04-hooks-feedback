eb72fabbd5fdd4c3ebfd0e4a52e2acf2406a24ca
Denys Nazarenko <denysnazarenko23@gmail.com>
2021-10-19 17:59:29 +0300

    feedback options to hooks

--- src/components/Feedback/FeedbackOptions/FeedbackOptions.js
+++ src/components/Feedback/FeedbackOptions/FeedbackOptions.js
@@ -1,17 +1,13 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 
-export default class FeedbackOptions extends Component {
-  render() {
-    const { good, neutral, bad } = this.props;
-    return (
-      <>
-        <button onClick={good}>Good</button>
-        <button onClick={neutral}>Neutral</button>
-        <button onClick={bad}>Bad</button>
-      </>
-    );
-  }
+export default function FeedbackOptions({ good, neutral, bad }) {
+  return (
+    <>
+      <button onClick={good}>Good</button>
+      <button onClick={neutral}>Neutral</button>
+      <button onClick={bad}>Bad</button>
+    </>
+  );
 }
 
 FeedbackOptions.propTypes = {
