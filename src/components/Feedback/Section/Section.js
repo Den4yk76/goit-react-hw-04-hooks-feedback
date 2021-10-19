bae5a554cb6daacc1ba83d3d75f2d7ada7b0c54a
Denys Nazarenko <denysnazarenko23@gmail.com>
2021-10-19 18:14:39 +0300

    Section to hooks

--- src/components/Feedback/Section/Section.js
+++ src/components/Feedback/Section/Section.js
@@ -1,15 +1,12 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 
-export default class Section extends Component {
-  render() {
-    return (
-      <section>
-        <h2>{this.props.title ? this.props.title : 'Please leave feedback'}</h2>
-        {this.props.children}
-      </section>
-    );
-  }
+export default function Section({ title, children }) {
+  return (
+    <section>
+      <h2>{title ? title : 'Please leave feedback'}</h2>
+      {children}
+    </section>
+  );
 }
 
 Section.propTypes = {
