ef62b9be57c963f39015fe05eb3720392a2e09d6
Denys Nazarenko <denysnazarenko23@gmail.com>
2021-10-19 18:12:58 +0300

    Notofication to hooks

--- src/components/Feedback/Notification/Notification.js
+++ src/components/Feedback/Notification/Notification.js
@@ -1,10 +1,7 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 
-export default class Notification extends Component {
-  render() {
-    return this.props.message ? this.props.message : 'No information ';
-  }
+export default function Notification({ message }) {
+  return message ? message : 'No information ';
 }
 
 Notification.propTypes = {
